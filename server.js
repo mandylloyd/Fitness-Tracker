78f0c5dc33b212bfbd1d437256629e4ecd93debe
honey bee <honeybee@users.noreply.github.com>
2020-10-20 17:37:27 -0400

    set up express app and connected mongoose

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 // dependencies
+const express = require("express");
+const mongoose = require("mongoose");
 
 // express app
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
 
 //connect with mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
-// create html and api routes
+// still need to create html and api routes !!!!
 
 //listen
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
